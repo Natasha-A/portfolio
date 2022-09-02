a819a69a255a74fd41b71d4a7b124fa19ac57c96
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-09-02 16:27:18 -0400

    style project cards

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -59,12 +59,12 @@ export const Work = () => {
               transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
               className="app__work-hover app__flex"
             >
-              <a href={work.projectLink} target="_blank" rel="norefer">
+              <a href={work.projectLink} target="_blank" rel="noreferrer">
                 <motion.div
                 whileInView={{scale: [0,1]}}
                  whileHover={{scale:[1,0.9]}}
                  transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
-                 className="app__work-hover app__flex"
+                 className="app__flex"
                 >
                   <AiFillEye />
                 </motion.div>
@@ -74,7 +74,7 @@ export const Work = () => {
                 whileInView={{scale: [0,1]}}
                  whileHover={{scale:[1,0.9]}}
                  transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
-                 className="app__work-hover app__flex"
+                 className="app__flex"
                 >
                   <AiFillGithub />
                 </motion.div>
